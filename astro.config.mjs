19230b07eba329c81423915cca5cbefa44928019
Tomoaki Hirabayashi <tomoaki.hirabayashi@medley.jp>
2025-11-07 01:13:29 +0900

    feat: add assetsignore and wrangler.jsonc

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,18 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   prefetch:{
     prefetchAll: true,
     defaultStrategy: "viewport"
-  }
+  },
+
+  adapter: cloudflare()
 });
