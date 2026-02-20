a3fdf850dce36efef5c57158b8bbd9c0f2760973
Tomoaki Hirabayashi <tomoaki.hirabayashi@medley.jp>
2026-02-20 13:40:43 +0900

    fix layout

---
+++ src/constants/links.ts
@@ -0,0 +1,18 @@
+export const links = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "About",
+    href: "/about",
+  },
+  {
+    label: "Work",
+    href: "/work",
+  },
+  {
+    label: "Blog",
+    href: "/blog",
+  },
+] as const;
